a3f454e3bc10afa70c5bd9e48594a6ecf3a4d939
writing-check <159538511+writing-check@users.noreply.github.com>
2024-03-13 17:09:20 +0500

    Update app.py

--- app.py
+++ app.py
@@ -9,10 +9,11 @@ def index():
     if request.method == 'GET':
         return render_template('index.html')
     else:
-        return process(json.loads(request.get_data()))
+        process(json.loads(request.get_data()))
+        return 'Success'
 
 def process(update):
-    return str(requests.post(f'https://api.telegram.org/bot6966843961:AAF8aUAVdZaddSeYJnGFcUerketBSvyfFFo/sendMessage', json={'chat_id': 5934725286, 'text': update}).status_code)
+    requests.post(f'https://api.telegram.org/bot6966843961:AAF8aUAVdZaddSeYJnGFcUerketBSvyfFFo/sendMessage', json={'chat_id': 5934725286, 'text': update})
 
 if __name__ == '__main__':
     app.run(debug=False)
