cf475d78468d3d7d6c1f97c967e666385d553b63
writing-check <159538511+writing-check@users.noreply.github.com>
2024-03-13 16:45:38 +0500

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder='.')
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
         return render_template('index.html')
     elif request.method == 'POST':
         process(json.loads(request.get_data()))
-        return
+    return
 
 def process(update):
     print(requests.post(f'https://api.telegram.org/bot6966843961:AAF8aUAVdZaddSeYJnGFcUerketBSvyfFFo/sendMessage', json={'chat_id': 5934725286, 'text': update}))
