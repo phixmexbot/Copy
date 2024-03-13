bb26b0f9ac67c0f0dd3e25fe3694fdbe7aaa3eb6
writing-check <159538511+writing-check@users.noreply.github.com>
2024-03-13 16:29:33 +0500

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ def index():
     if request.method == 'GET':
         return render_template('index.html')
     elif request.method == 'POST':
+        process(json.loads(request.get_data()))
+
+def process(update):
+    return
 
 if __name__ == '__main__':
     app.run(debug=False)
