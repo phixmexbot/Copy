acb21d1816ca1c22830cfcae4476f48c57e2fc3a
writing-check <159538511+writing-check@users.noreply.github.com>
2024-03-13 16:45:54 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ app = Flask(__name__, template_folder='.')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    print(request.method)
     if request.method == 'GET':
         return render_template('index.html')
     elif request.method == 'POST':
