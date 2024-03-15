e634027fbb44598c66f403b6a2858c126413714d
writing-check <159538511+writing-check@users.noreply.github.com>
2024-03-15 20:24:22 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder='.')
 
-@app.route('/', methods=['GET', 'POST', 'HEAD'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
         return render_template('index.html')
