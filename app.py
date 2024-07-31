afb34f288761ae290f6be9af476fc454cf567f0b
phixmexbot <167516711+phixmexbot@users.noreply.github.com>
2024-07-31 23:06:13 +0500

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ CONNECTION = 'l5fqrAiviEj0CAAALAJbw05zldA'
 USERNAME = 'look'
 PASSWORD = 'eternal'
 REACTIONS = ['👍', '🔥', '❤️', '👏', '🕊']
-DIRECTORIES = ['home', 'about', 'copyright', 'html/initial', 'happy-birthday', 'activities/lightening-flash', 'activities/orbit-around', 'activities/doodle-rain', 'activities/angry-birds', 'services/chat-bot', 'services/home-tab', 'projects/automatic-attendance', 'projects/ai-navigator']
+DIRECTORIES = ['home', 'about', 'copyright', 'templates/initial', 'activities/lightening-flash', 'activities/orbit-around', 'activities/doodle-rain', 'activities/angry-birds', 'services/chat-bot', 'services/home-tab', 'projects/automatic-attendance', 'projects/ai-navigator']
 
 app = Flask(__name__, template_folder='.')
 
