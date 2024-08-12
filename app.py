66a8eb7af090860c8c39e6dcdac4f87296bf7dde
phixmexbot <167516711+phixmexbot@users.noreply.github.com>
2024-08-12 18:04:32 +0500

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def activate():
 @app.route('/', methods=['GET', 'POST', 'HEAD'])
 def index():
     if request.method == 'GET':
-        return render_template('initial.html')
+        return render_template('templates/initial.html')
     elif request.method == 'POST':
         process(json.loads(request.get_data()))
         return 'Success'
