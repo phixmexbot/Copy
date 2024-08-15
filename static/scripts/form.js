3af03a1f293afcecb1360810cde3b07cd016147f
phixmexbot <167516711+phixmexbot@users.noreply.github.com>
2024-08-15 16:26:37 +0500

    Update form.js

--- static/scripts/form.js
+++ static/scripts/form.js
@@ -1,8 +1,28 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const openButton = document.querySelector('#openButton');
-  const sendMessage = document.querySelector('#sendButton');
+const openButton = document.querySelector('#openButton');
+const sendButton = document.querySelector('#sendButton');
+const closeButton = document.querySelector('#closeButton');
+const rateElement = document.querySelector('#rate');
 
-  openButton.addEventListener('click', () => {
-    console.log('hello');
-  });
-});
+const addOpacity = () => {
+  if (rateElement) {
+    rateElement.style.opacity = '0';
+  }
+};
+
+const removeOpacity = () => {
+  if (rateElement) {
+    rateElement.style.opacity = '';
+  }
+};
+
+if (openButton) {
+  openButton.addEventListener('click', addOpacity);
+}
+
+if (sendButton) {
+  sendButton.addEventListener('click', removeOpacity);
+}
+
+if (closeButton) {
+  closeButton.addEventListener('click', removeOpacity);
+}
