4cf6c1a3670f8cdc86e369c5075396761f3915ba
phixmexbot <167516711+phixmexbot@users.noreply.github.com>
2024-08-16 13:46:21 +0500

    Update prefetch.js

--- static/scripts/prefetch.js
+++ static/scripts/prefetch.js
@@ -1,13 +1,16 @@
-        window.onload = function() {
-            // Ensure the data is properly escaped and parsed
-            const resources = JSON.parse('{{ static_files | tojson | safe }}');
-            resources.forEach(function(resource) {
-                const link = document.createElement('link');
-                link.rel = 'prefetch';
-                link.href = `/${resource}`;
-                link.as = resource.includes('.css') ? 'style' :
-                          resource.includes('.js') ? 'script' :
-                          resource.includes('.svg') ? 'image' : '';
-                document.head.appendChild(link);
-            });
-        };
+window.onload = function() {
+    try {
+        const resources = JSON.parse('{{ static_files | tojson | safe }}');
+        resources.forEach(function(resource) {
+            const link = document.createElement('link');
+            link.rel = 'prefetch';
+            link.href = `/${resource}`;
+            link.as = resource.includes('.css') ? 'style' :
+                      resource.includes('.js') ? 'script' :
+                      resource.includes('.svg') ? 'image' : '';
+            document.head.appendChild(link);
+        });
+    } catch (error) {
+        console.error('JSON Parsing Error:', error);
+    }
+};
