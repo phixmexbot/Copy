35c45810fc440b5445c6939caaead255a8e2186b
phixmexbot <167516711+phixmexbot@users.noreply.github.com>
2024-09-02 12:09:00 +0500

    Update rating.js

--- static/scripts/rating.js
+++ static/scripts/rating.js
@@ -3,27 +3,22 @@ Vue.component('starRating', {
   data: () => {
     return {
       rating: 0,
-      ratingSelected: false
-    };
+      ratingSelected: false };
+
   },
   methods: {
     changeRating: function (val) {
-      this.rating = val;
-      this.ratingSelected = true;
+      if (this.rating === 0) {
+        this.rating = val;
+        this.ratingSelected = true;
+      }
+    } },
 
-      // Set the title and message based on the selected rating
-      this.$emit('update:ratingTitle', `Thank you!`);
-      this.$emit('update:ratingMessage', `You rated this project ${val} ${this.starText}.`);
-    }
-  },
-  template: '#tpl_star_rating'
-});
+  template: '#tpl_star_rating' });
 
 var app = new Vue({
   el: '#rate',
   data: {
-    ratingTitle: 'Rate this project', // Initial message
-    ratingMessage: 'Please select a star to rate.', // Initial message
-    starText: 'star/s'
-  }
-});
+    ratingTitle: 'Thank you!',
+    ratingMessage: 'You rated this project:',
+    starText: 'star/s' } });
