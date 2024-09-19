76a4867c2b6654616682d8ed5fac9e25d501ec7d
phixmexbot <167516711+phixmexbot@users.noreply.github.com>
2024-09-20 01:05:56 +0500

    Update rain.js

--- static/scripts/rain.js
+++ static/scripts/rain.js
@@ -1,28 +1,112 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const doodle = document.querySelector("css-doodle");
-    let rainStarted = false;
 
-    function startRainAnimation() {
-        if (!rainStarted) {
-            doodle.style.display = "block"; // Show the doodle element
-            doodle.style.opacity = "1"; // Set the opacity to 1 to make the doodle visible
-            rainStarted = true;
+var c = document.getElementById("canvas-club");
+var ctx = c.getContext("2d");
+var w = c.width = window.innerWidth;
+var h = c.height = window.innerHeight;
+var clearColor = 'rgba(0, 0, 0, 0)'; // Transparent background
+var max = 1000;
+var drops = [];
+
+function random(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+function O() {}
+
+O.prototype = {
+    init: function() {
+        this.x = random(0, w);
+        this.y = 0;
+        this.color = '#FFFFFF';
+        this.w = 2;
+        this.h = 1;
+        this.vy = random(4, 5);
+        this.vw = 3;
+        this.vh = 1;
+        this.size = 2;
+        this.hit = random(h * 0.89, h);
+        this.a = 1;
+        this.va = .96;
+    },
+    draw: function() {
+        if (this.y > this.hit) {
+            ctx.beginPath();
+            ctx.moveTo(this.x, this.y - this.h / 2);
+
+            ctx.bezierCurveTo(
+                this.x + this.w / 2, this.y - this.h / 2,
+                this.x + this.w / 2, this.y + this.h / 2,
+                this.x, this.y + this.h / 2
+            );
+
+            ctx.bezierCurveTo(
+                this.x - this.w / 2, this.y + this.h / 2,
+                this.x - this.w / 2, this.y - this.h / 2,
+                this.x, this.y - this.h / 2
+            );
+
+            ctx.strokeStyle = 'rgba(255, 255, 255, ' + this.a + ')'; 
+            ctx.stroke();
+            ctx.closePath();
+
+        } else {
+            ctx.fillStyle = this.color;
+            ctx.fillRect(this.x, this.y, this.size, this.size * 3); // Adjust this multiplier
+        }
+        this.update();
+    },
+    update: function() {
+        if(this.y < this.hit){
+            this.y += this.vy;
+        } else {
+            if(this.a > .03){
+                this.w += this.vw;
+                this.h += this.vh;
+                if(this.w > 100){
+                    this.a *= this.va;
+                    this.vw *= .98;
+                    this.vh *= .98;
+                }
+            } else {
+                this.init();
+            }
         }
     }
+}
 
-    function checkHashAndStartAnimation() {
-        if (window.location.hash === "#rain" && !rainStarted) {
-            startRainAnimation();
-        }
+function resize(){
+    w = c.width = window.innerWidth;
+    h = c.height = window.innerHeight;
+}
+
+function setup(){
+    drops = []; // Reset the array to avoid duplication
+    for(var i = 0; i < max; i++){
+        (function(j){
+            setTimeout(function(){
+                var o = new O();
+                o.init();
+                drops.push(o);
+            }, j * 200);
+        }(i));
     }
+}
 
-    // Initially hide the doodle element by setting display to "none" and opacity to 0
-    doodle.style.display = "none";
-    doodle.style.opacity = "0";
+function anim() {
+    ctx.clearRect(0, 0, w, h); // Clears the canvas without filling it with a color
+    for(var i in drops){
+        drops[i].draw();
+    }
+    requestAnimationFrame(anim);
+}
 
-    // Check the hash value when the page loads
-    checkHashAndStartAnimation();
+window.addEventListener("resize", resize);
 
-    // Listen for hash changes
-    window.addEventListener("hashchange", checkHashAndStartAnimation);
-});
+function onHashChange() {
+    if (location.hash === '#rain') {
+        setup();
+        anim();
+    }
+}
+
+window.addEventListener("hashchange", onHashChange);
