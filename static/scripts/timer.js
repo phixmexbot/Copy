227aff5678fc71f7e7d9ad70c85a4ade866291d7
phixmexbot <167516711+phixmexbot@users.noreply.github.com>
2024-10-19 23:28:56 +0500

    Update timer.js

--- static/scripts/timer.js
+++ static/scripts/timer.js
@@ -1,3 +1,5 @@
-setTimeout(function() {
-  window.location.href = '/home';
-}, 10000);
+document.addEventListener('DOMContentLoaded', function() {
+  setTimeout(function() {
+    window.location.href = '/home';
+  }, 10000); // Redirect after 10 seconds
+});
